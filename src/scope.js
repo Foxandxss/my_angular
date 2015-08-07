9e92eb3521b0c7aedc3abc911aea2a99f71b3e07
Jesus Rodriguez <foxandxss@gmail.com>
2015-08-07 13:40:38 +0200

    Separated watches

--- src/scope.js
+++ src/scope.js
@@ -150,6 +150,7 @@ Scope.prototype.$new = function() {
   var ChildScope = function() { };
   ChildScope.prototype = this;
   var child = new ChildScope();
+  child.$$watchers = [];
   return child;
 };
 
