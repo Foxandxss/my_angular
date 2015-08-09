57c3d9557119878d710d2f3ee35685a75ca372ce
Jesus Rodriguez <foxandxss@gmail.com>
2015-08-09 20:36:11 +0200

    Parsing integers

--- src/parse.js
+++ src/parse.js
@@ -5,16 +5,63 @@ function Lexer() {
 
 }
 
-Lexer.prototype.lex = function(text) {
+Lexer.prototype.isNumber = function(ch) {
+  return '0' <= ch && ch <= '9';
+};
 
+Lexer.prototype.lex = function(text) {
+  this.text = text;
+  this.index = 0;
+  this.ch = undefined;
+  this.tokens = [];
+
+  while (this.index < this.text.length) {
+    this.ch = this.text.charAt(this.index);
+    if (this.isNumber(this.ch)) {
+      this.readNumber();
+    } else {
+      throw 'Unexpected next character: ' + this.ch;
+    }
+  }
+
+  return this.tokens;
+};
+
+Lexer.prototype.readNumber = function() {
+  var number = '';
+  while (this.index < this.text.length) {
+    var ch = this.text.charAt(this.index);
+    if (this.isNumber(ch)) {
+      number += ch;
+    } else {
+      break;
+    }
+    this.index++;
+  }
+  this.tokens.push({
+    text: number,
+    value: Number(number)
+  });
 };
 
 function AST(lexer) {
   this.lexer = lexer;
 }
 
+AST.Literal = 'Literal';
+AST.Program = 'Program';
+
 AST.prototype.ast = function(text) {
   this.tokens = this.lexer.lex(text);
+  return this.program();
+};
+
+AST.prototype.constant = function() {
+  return {type: AST.Literal, value: this.tokens[0].value};
+};
+
+AST.prototype.program = function() {
+  return {type: AST.Program, body: this.constant()};
 };
 
 function ASTCompiler(astBuilder) {
@@ -23,6 +70,22 @@ function ASTCompiler(astBuilder) {
 
 ASTCompiler.prototype.compile = function(text) {
   var ast = this.astBuilder.ast(text);
+  this.state = {body: []};
+  this.recurse(ast);
+
+  /* jshint -W054 */
+  return new Function(this.state.body.join(''));
+  /* jshint +W054 */
+};
+
+ASTCompiler.prototype.recurse = function(ast) {
+  switch (ast.type) {
+    case AST.Program:
+      this.state.body.push('return ', this.recurse(ast.body), ';');
+      break;
+    case AST.Literal:
+      return ast.value;
+  }
 };
 
 function Parser(lexer) {
