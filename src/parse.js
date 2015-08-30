c2400089a522e76ae2b0844c63011f8b09cc234f
Jesus Rodriguez <foxandxss@gmail.com>
2015-08-30 14:13:39 +0200

    Ensuring safe functions

--- src/parse.js
+++ src/parse.js
@@ -4,6 +4,21 @@
 var ESCAPES = {'n':'\n', 'f':'\f', 'r':'\r', 't':'\t',
   'v':'\v', '\'':'\'', '"':'"'};
 
+var CALL = Function.prototype.call;
+var APPLY = Function.prototype.apply;
+var BIND = Function.prototype.bind;
+
+function ensureSafeFunction(obj) {
+  if (obj) {
+    if (obj.constructor === obj) {
+      throw 'Referencing Function in Angular expressions is disallowed!';
+    } else if (obj === CALL || obj === APPLY || obj === BIND) {
+      throw 'Referencing call, apply, or bind in Angular expressions is disallowed!';
+    }
+  }
+  return obj;
+}
+
 function ensureSafeMemberName(name) {
   if (name === 'constructor' || name === '__proto__' ||
       name === '__defineGetter__' || name === '__defineSetter__' ||
@@ -340,6 +355,10 @@ function ASTCompiler(astBuilder) {
   this.astBuilder = astBuilder;
 }
 
+ASTCompiler.prototype.addEnsureSafeFunction = function(expr) {
+  this.state.body.push('ensureSafeFunction(' + expr + ');');
+};
+
 ASTCompiler.prototype.addEnsureSafeMemberName = function(expr) {
   this.state.body.push('ensureSafeMemberName(' + expr + ');');
 };
@@ -369,9 +388,11 @@ ASTCompiler.prototype.compile = function(text) {
   return new Function(
     'ensureSafeMemberName',
     'ensureSafeObject',
+    'ensureSafeFunction',
     fnString)(
     ensureSafeMemberName,
-    ensureSafeObject);
+    ensureSafeObject,
+    ensureSafeFunction);
   /* jshint +W054 */
 };
 
@@ -445,6 +466,7 @@ ASTCompiler.prototype.recurse = function(ast, context, create) {
           callee = this.nonComputedMember(callContext.context, callContext.name);
         }
       }
+      this.addEnsureSafeFunction(callee);
       return callee + '&&ensureSafeObject(' + callee + '(' + args.join(',') + '))';
     case AST.Identifier:
       ensureSafeMemberName(ast.name);
