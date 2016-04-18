d325e5027a89d8b252459a669068a9b07bb1ce61
Foxandxss <foxandxss@gmail.com>
2016-04-18 20:01:55 +0200

    The $q provider

--- test/angular_public_spec.js
+++ test/angular_public_spec.js
@@ -36,12 +36,12 @@ describe('angularPublic', function() {
     expect(injector.has('$rootScope')).toBe(true);
   });
 
-  // it('sets up $q', function() {
-  //   publishExternalAPI();
-  //   var injector = createInjector(['ng']);
-  //   expect(injector.has('$q')).toBe(true);
-  // });
-  //
+  it('sets up $q', function() {
+    publishExternalAPI();
+    var injector = createInjector(['ng']);
+    expect(injector.has('$q')).toBe(true);
+  });
+
   // it('sets up $http and $httpBackend', function() {
   //   publishExternalAPI();
   //   var injector = createInjector(['ng']);
