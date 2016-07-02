d49524956163ffe945d519933f56641990182fc9
Foxandxss <foxandxss@gmail.com>
2016-07-02 19:01:56 +0200

    Resolving a deferred

--- src/q.js
+++ src/q.js
@@ -1,24 +1,43 @@
 'use strict';
 
 function $QProvider() {
-  this.$get = function() {
-
-    function Promise() {
-
-    }
+  this.$get = ['$rootScope', function($rootScope) {
 
     function Deferred() {
       this.promise = new Promise();
     }
 
+    Deferred.prototype.resolve = function(value) {
+      this.promise.$$state.value = value;
+      scheduleProcessQueue(this.promise.$$state);
+    };
+
+    function Promise() {
+      this.$$state = {};
+    }
+
+    Promise.prototype.then = function(onFulfilled) {
+      this.$$state.pending = onFulfilled;
+    };
+
     function defer() {
       return new Deferred();
+    }
+
+    function processQueue(state) {
+      state.pending(state.value);
+    }
+
+    function scheduleProcessQueue(state) {
+      $rootScope.$evalAsync(function() {
+        processQueue(state);
+      });
     }
 
     return {
       defer: defer
     };
-  };
+  }];
 }
 
 module.exports = $QProvider;
