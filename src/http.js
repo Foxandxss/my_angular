09fe9cade2fdffd5dcee3e8be396e12d09ea6de6
Jesus Rodriguez <Foxandxss@gmail.com>
2017-01-15 16:04:00 +0100

    JSON serialization and parsing

--- src/http.js
+++ src/http.js
@@ -2,6 +2,36 @@
 
 var _ = require('lodash');
 
+function defaultHttpResponseTransform(data, headers) {
+  if (_.isString(data)) {
+    var contentType = headers('Content-Type');
+    if (contentType && contentType.indexOf('application/json') === 0 || isJsonLike(data)) {
+      return JSON.parse(data);
+    }
+  }
+  return data;
+}
+
+function isBlob(object) {
+  return object.toString() === '[object Blob]';
+}
+
+function isFile(object) {
+  return object.toString() === '[object File]';
+}
+
+function isFormData(object) {
+  return object.toString() === '[object FormData]';
+}
+
+function isJsonLike(data) {
+  if (data.match(/^\{(?!\{)/)) {
+    return data.match(/\}$/);
+  } else if (data.match(/^\[/)) {
+    return data.match(/\]$/);
+  }
+}
+
 function $HttpProvider() {
 
   var defaults = this.defaults = {
@@ -18,7 +48,15 @@ function $HttpProvider() {
       patch: {
         'Content-Type': 'application/json;charset=utf-8'
       }
-    }
+    },
+    transformRequest: [function(data) {
+      if (_.isObject(data) && !isBlob(data) && !isFile(data) && !isFormData(data)) {
+        return JSON.stringify(data);
+      } else {
+        return data;
+      }
+    }],
+    transformResponse: [defaultHttpResponseTransform]
   };
 
   this.$get = ['$httpBackend', '$q', '$rootScope', function($httpBackend, $q, $rootScope) {
