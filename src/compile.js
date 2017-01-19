0a29786acc13349947bdfcd778108afa05eedf06
Jesus Rodriguez <Foxandxss@gmail.com>
2017-01-20 00:44:13 +0100

    Registering directives

--- src/compile.js
+++ src/compile.js
@@ -1,9 +1,37 @@
 'use strict';
 
-function $CompileProvider() {
+var _ = require('lodash');
+
+function $CompileProvider($provide) {
+
+  var hasDirectives = {};
+
+  this.directive = function(name, directiveFactory) {
+    var self = this;
+
+    if (_.isString(name)) {
+      if (name === 'hasOwnProperty') {
+        throw 'hasOwnProperty is not a valid directive name';
+      }
+      if (!hasDirectives.hasOwnProperty(name)) {
+        hasDirectives[name] = [];
+        $provide.factory(name + 'Directive', ['$injector', function($injector) {
+          var factories = hasDirectives[name];
+          return _.map(factories, $injector.invoke);
+        }]);
+      }
+      hasDirectives[name].push(directiveFactory);
+    } else {
+      _.forEach(name, function(directiveFactory, name) {
+        self.directive(name, directiveFactory);
+      });
+    }
+  };
+
   this.$get = function() {
 
   };
 }
+$CompileProvider.$inject = ['$provide'];
 
 module.exports = $CompileProvider;
